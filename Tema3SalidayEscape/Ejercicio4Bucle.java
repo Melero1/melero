b55a78e719ff4d081496792b51aeb33d0a04f756
Melero1 <147091703+Melero1@users.noreply.github.com>
2023-10-19 17:50:22 +0200

    Update Ejercicio4Bucle.java

--- Tema3SalidayEscape/Ejercicio4Bucle.java
+++ Tema3SalidayEscape/Ejercicio4Bucle.java
@@ -3,8 +3,7 @@ public class Ejercicio4Bucle {
     public static void main (String[] args) {
         String numeros = " ";
         for (int i = 1; i<10; i++) {
-            numeros += i; 
-            System.out.print(i);
+            numeros += i;
             System.out.printf("%s\n", numeros);
         }
        
